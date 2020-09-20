a564ff5853084baeb972d7fcf1195be0da3dd5a6
KodaKDominus <kodakatsuro@gmail.com>
2020-09-20 20:28:49 +1000

    Test

--- auction/__init__.py
+++ auction/__init__.py
@@ -9,5 +9,4 @@ def create_app():
     app.register_blueprint(views.mainbp)
     app.register_blueprint(listings.listingbp)
 
-
     return app
--- auction/views.py
+++ auction/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+# Create main blueprint
 mainbp = Blueprint('main', __name__)
 
 @mainbp.route('/')
