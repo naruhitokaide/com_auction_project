85bf0732c3fef07dd4839b528b8c56dc02b86a95
KodaKDominus <kodakatsuro@gmail.com>
2020-09-25 16:32:10 +1000

    Add forms.py

---
+++ auction/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.validators import InputRequired, Length, Email, EqualTo
+
+#creates the login information
+class LoginForm(FlaskForm):
+    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
+    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    submit = SubmitField("Login")
+
+ # this is the registration form
+class RegisterForm(FlaskForm):
+    user_name=StringField("User Name", validators=[InputRequired()])
+    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    
+    #add buyer/seller - check if it is a buyer or seller hint : Use RequiredIf field
+
+
+    #linking two fields - password should be equal to data entered in confirm
+    password=PasswordField("Password", validators=[InputRequired(),
+                  EqualTo('confirm', message="Passwords should match")])
+    confirm = PasswordField("Confirm Password")
+
+    #submit button
+    submit = SubmitField("Register")
