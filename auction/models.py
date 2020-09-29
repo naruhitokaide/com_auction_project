5243ebecb4902c2658cbfd3cec17579a14d337c2
KodaKDominus <kodakatsuro@gmail.com>
2020-09-29 16:36:48 +1000

    Added WatchListItem class

--- auction/models.py
+++ auction/models.py
@@ -36,19 +36,6 @@ class Listing(db.Model):
 
     reviews = db.relationship('Review', backref = 'listing')
 
-    # def __init__(self, title, starting_bid, description, status, condition, image, end_date, seller):
-    #     self.title = title
-    #     self.starting_bid = starting_bid
-    #     self.current_bid = starting_bid
-    #     self.total_bids = 0
-    #     self.description = description
-    #     self.condition = condition
-    #     self.status = status
-    #     self.image = image
-    #     self.end_date = end_date
-    #     self.seller = seller
-    #     self.reviews = list()
-
     def set_review(self, review):
         self.reviews.append(review)
 
@@ -66,13 +53,15 @@ class Review(db.Model):
         debug_string = 'User: {}, Title: {}, Feedback: {}'
         debug_string.format(self.user, self.title, self.feedback)
         return debug_string
-    
-    # def __init__ (self, user, title, feedback):
-    #     self.user = user
-    #     self.title = title
-    #     self.feedback = feedback
 
-    
 
+class WatchListItem(db.Model):
+    __tablename__ = 'watchlistitem'
+    id = db.Column(db.Integer, primary_key=True)
+    date_added = db.Column(db.DateTime, default = datetime.now())
+
+    # Foreign Keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    listing_id = db.Column(db.Integer, db.ForeignKey('listings.id'))
 
 
