29ec8bd917deb6eb959bd49f23f3c849201f6538
KodaKDominus <kodakatsuro@gmail.com>
2020-09-29 14:45:32 +1000

    Comment out get_listing()

--- auction/listings.py
+++ auction/listings.py
@@ -24,27 +24,32 @@ def createlisting():
 
   return render_template('listings/createlisting.html', form=form)
 
-def get_listing():
 
-  # For testing purposes to make sure showlisting.html is updating dynmically 
 
-  listing_description= """ 
-    The laptop is in excellent condition with only 61 battery cycles.
 
-    Screen is in like new condition, no scratched marks or dead pixels absolutely perfect.
+# Testing purposes:  
 
-    Keyboard perfectly functional There is a small chip in the Command Key (see Image) I don't know what has caused it, everything else is perfect condition."""
+# def get_listing():
+
+#   # For testing purposes to make sure showlisting.html is updating dynmically 
+
+#   listing_description= """ 
+#     The laptop is in excellent condition with only 61 battery cycles.
+
+#     Screen is in like new condition, no scratched marks or dead pixels absolutely perfect.
+
+#     Keyboard perfectly functional There is a small chip in the Command Key (see Image) I don't know what has caused it, everything else is perfect condition."""
   
-  image_location = 'https://images.unsplash.com/photo-1585247226801-bc613c441316?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
+#   image_location = 'https://images.unsplash.com/photo-1585247226801-bc613c441316?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80'
   
-  listing = Listing('Testing New Lising', '500', listing_description, 'Active', 'Used', image_location, '19/08/2020', 'User1')
+#   listing = Listing('Testing New Lising', '500', listing_description, 'Active', 'Used', image_location, '19/08/2020', 'User1')
 
-  review = Review('User1', 'Great device for school!', 'Awesome computer')
-  review2 = Review('User2', 'Amazing!', 'Great so far')
+#   review = Review('User1', 'Great device for school!', 'Awesome computer')
+#   review2 = Review('User2', 'Amazing!', 'Great so far')
 
-  listing.set_review(review)
-  listing.set_review(review2)
+#   listing.set_review(review)
+#   listing.set_review(review2)
 
-  return listing
+#   return listing
 
 
