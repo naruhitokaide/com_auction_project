682e675ae9e7127bd78362d1acb8620afe2318d3
KodaKDominus <kodakatsuro@gmail.com>
2020-10-07 11:44:48 +1000

    Search computers by its title

--- auction/views.py
+++ auction/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Listing
 
 # Create main blueprint
@@ -8,3 +8,14 @@ mainbp = Blueprint('main', __name__)
 def index():
     listings = Listing.query.all()
     return render_template('index.html', listings=listings)
+
+@mainbp.route('/search')
+def search():
+    #get the search string from request
+    if request.args['search']:
+        item = "%" + request.args['search'] + '%'
+    #use filter and like function to search for matching item
+        listing = Listing.query.filter(Listing.title.like(item)).all()
+        return render_template('index.html', listings=listing)
+    else:
+        return redirect(url_for('main.index'))
