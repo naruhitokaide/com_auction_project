827ff0db77c5aeac936337b165142e1a08e3183e
KodaKDominus <kodakatsuro@gmail.com>
2020-10-20 14:27:23 +1000

    Revert "In Progress - Watchlist"
    This reverts commit 422c7293327e6cda0335f1040272f4e68650a79c.

--- auction/listings.py
+++ auction/listings.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, session, url_for, redirect
-from .models import Listing, Review, WatchListItem
+from .models import Listing, Review
 from auction.forms import ListingForm, ReviewForm
 from . import db
 from werkzeug.utils import secure_filename
@@ -75,11 +75,7 @@ def mylistings():
 
 @listingbp.route('/watchlist', methods=['GET', 'POST'])
 @login_required
-def watchlist(listing):
-  item  = Listing.query.filter_by(id=listing).first()
-  watchlist_item = WatchListItem(listing_id = item.id, user = current_user)
-  db.session.add(watchlist_item)
-  db.session.commit()
+def watchlist():
   return render_template('listings/watchlist.html')
 
 
