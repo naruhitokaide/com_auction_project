dfab2057225b3e9cb85f8e91b01f01a3751ea2d2
KodaKDominus <kodakatsuro@gmail.com>
2020-10-20 15:05:18 +1000

    Revert "Merge branch 'master' of https://github.com/ben04rogers/IAB207_ASSESSMENT_3"
    This reverts commit f054b0afe051349bbe4fb0379a8e67cfe8dec079, reversing
    changes made to 1e198877907685012cff02b030bfb77e271842a3.

--- auction/listings.py
+++ auction/listings.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, session, url_for, redirect
-from .models import Listing, Review
-from auction.forms import ListingForm, ReviewForm
+from flask import Blueprint, render_template, request, session, url_for, redirect, flash
+from .models import Listing, Review, Bid
+from auction.forms import ListingForm, ReviewForm, BidForm
 from . import db
 from werkzeug.utils import secure_filename
 import os
@@ -14,33 +14,34 @@ listingbp = Blueprint('listing', __name__, url_prefix='/listings')
 def showlisting(id):
     listing = Listing.query.filter_by(id=id).first()
     review_form_instance = ReviewForm()
-    return render_template('listings/showlisting.html', listing=listing, form=review_form_instance)
+    bid_form_instance = BidForm()
 
+    return render_template('listings/showlisting.html', listing=listing, form=review_form_instance, bidform=bid_form_instance)
 
 @listingbp.route('/create', methods=['GET', 'POST'])
 @login_required #login is required for creating a listing
 def createlisting():
-  form = ListingForm()
+  listingform = ListingForm()
 
-  if form.validate_on_submit():
+  if listingform.validate_on_submit():
     #Check the image file
-    db_file_path = check_upload_file(form)
+    db_file_path = check_upload_file(listingform)
 
     # Write data to database
     listing = Listing()
     
-    listing.title = form.title.data
-    listing.starting_bid = form.starting_bid.data
-    listing.current_bid = form.starting_bid.data
+    listing.title = listingform.title.data
+    listing.starting_bid = listingform.starting_bid.data
+    listing.current_bid = listingform.starting_bid.data
     listing.total_bids = 0
-    listing.brand = form.brand.data
-    listing.cpu =  form.cpu.data
-    listing.ram_gb = form.ram.data
-    listing.storage_gb = form.storage.data
-    listing.condition = form.condition.data
-    listing.end_date = form.end_date.data
+    listing.brand = listingform.brand.data
+    listing.cpu =  listingform.cpu.data
+    listing.ram_gb = listingform.ram.data
+    listing.storage_gb = listingform.storage.data
+    listing.condition = listingform.condition.data
+    listing.end_date = listingform.end_date.data
     listing.status = 'Active'
-    listing.description = form.description.data
+    listing.description = listingform.description.data
     listing.image_url = db_file_path
     listing.seller = current_user.name
   
@@ -55,7 +56,7 @@ def createlisting():
   else:
     print('form is not valid')
 
-  return render_template('listings/createlisting.html', form=form)
+  return render_template('listings/createlisting.html', form=listingform)
 
 def check_upload_file(form):
   fp = form.image_url.data
@@ -70,7 +71,18 @@ def check_upload_file(form):
 @listingbp.route('/mylistings', methods=['GET', 'POST'])
 @login_required
 def mylistings():
-  return render_template('listings/mylistings.html')
+  listings = Listing.query.filter_by(seller=current_user.name).all()
+  return render_template('listings/mylistings.html', listings=listings)
+
+@listingbp.route('/mylistings/<listing>/close', methods=['GET', 'POST'])
+def close_listing(listing):
+   update_listing = Listing.query.filter_by(id=listing).first()
+   update_listing.status = 'Closed' 
+   db.session.commit()
+
+   # Re-render page with updated items 
+   listings = Listing.query.filter_by(seller=current_user.name).all()
+   return render_template('listings/mylistings.html', listings=listings)
 
 
 @listingbp.route('/watchlist', methods=['GET', 'POST'])
@@ -91,4 +103,39 @@ def review(listing):
       db.session.add(review)
       db.session.commit()
       print('Thank you for submitting a review of this item', 'success') 
+    return redirect(url_for('listing.showlisting', id=listing))
+
+@listingbp.route('/<listing>/bid', methods = ['GET', 'POST'])  
+@login_required
+def placebid(listing):
+    bidform = BidForm()
+    listing_obj = Listing.query.filter_by(id=listing).first()
+    
+
+    if bidform.validate_on_submit():  
+      # Write to database
+      bid = Bid()
+      bid.bid_amount = bidform.bid_amount.data
+      bid.listing_id = listing_obj.id
+      bid.bidder_name = current_user.name
+
+      # Check if bid is more than current bid 
+      if bid.bid_amount > listing_obj.current_bid:
+        listing_obj.current_bid = bid.bid_amount
+
+        # Update total bids 
+        update_total_bids = Listing.query.filter_by(id=listing).first()
+        update_total_bids.total_bids += 1
+        
+        flash("Bid was successfully placed!", 'success')
+      else: 
+        flash ("Bid amount has to be higher than current bid")
+          
+
+      db.session.add(bid)
+      
+      db.session.commit()
+      print('Bid was successfully placed!', 'success') 
+    else: 
+      print('Bid form is not valid')
     return redirect(url_for('listing.showlisting', id=listing))
