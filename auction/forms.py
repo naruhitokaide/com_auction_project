8402a0bc2882c919c4f17879fd449b850adbb079
KodaKDominus <kodakatsuro@gmail.com>
2020-10-27 19:11:58 +1000

    Add placeholders in create listing form

--- auction/forms.py
+++ auction/forms.py
@@ -26,18 +26,18 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Register")
 
 class ListingForm(FlaskForm):
-    title = StringField('Listing Title', validators=[InputRequired('Listing title is required')])
-    starting_bid = DecimalField('Starting Bid (Decimal format)', validators=[InputRequired('Must enter a starting bid')])
+    title = StringField('Listing Title', validators=[InputRequired('Listing title is required')], render_kw={"placeholder": "Title..."})
+    starting_bid = DecimalField('Starting Bid', validators=[InputRequired('Must enter a starting bid')], render_kw={"placeholder": "$"})
     brand = SelectField('Brand', choices=[('Apple', 'Apple'), ('Microsoft', 'Microsoft'), ('Dell', 'Dell'), ('HP', 'HP'), ('Lenovo', 'Lenovo'), ('Acer', 'Acer')])
     cpu = SelectField('CPU', choices=[('i3', 'i3'), ('i5', 'i5'), ('i7', 'i7'), ('Ryzen 3', 'Ryzen 3'), ('Ryzen 5', 'Ryzen 5'), ('Ryzen 7', 'Ryzen 7')])
     ram = SelectField('RAM', choices=[('4GB', '4GB'), ('8GB', '8GB'), ('16GB', '16GB'), ('32GB', '32GB')])
     storage = SelectField('Storage', choices=[('64GB', '64GB'), ('128GB', '128GB'), ('256GB', '256GB'), ('512GB', '512GB'), ('1TB', '1TB')])
     condition = SelectField('Condition', choices=[('Excellent', 'Excellent'), ('Good', 'Good'), ('Minor defects', 'Minor defects'), ('Used', 'Used'), ('New', 'New')])
-    end_date =  DateField('End Date (dd/mm/yyyy)', format='%d/%m/%Y')
+    end_date =  DateField('End Date', format='%d/%m/%Y', render_kw={"placeholder": "dd/mm/yyyy"})
     ALLOWED_FILE = {'png', 'jpg', 'JPG', 'PNG', 'jpeg'}
     image_url = FileField('Image', validators=[
         FileRequired(message='Image can not be empty'), FileAllowed(ALLOWED_FILE, message='Only supports png, jpg, JPG, PNG')])
-    description = TextAreaField('Description', validators=[InputRequired('Description is required'), Length(min=10, max=300, message='Description is too short or too long')])
+    description = TextAreaField('Description', validators=[InputRequired('Description is required'), Length(min=10, max=300, message='Description is too short or too long')], render_kw={"placeholder": "Description of listing..."})
     submit = SubmitField('Post Listing')
 
 class ReviewForm(FlaskForm):
